ded998faad743b05383eff60b0bb7664ceb53d75
Pradeep Kumar Jayaraman <pradeep.kumar.jayaraman@autodesk.com>
2019-12-27 19:33:15 -0500

    Add example using viewer API

--- test/HelloWorld/HelloWorld.cpp
+++ test/HelloWorld/HelloWorld.cpp
@@ -3,6 +3,14 @@
 int main()
 {
     rcube::viewer::RCubeViewer viewer;
+    viewer.addIcoSphereSurface("icoSphere1", 1.0, 4);
+    rcube::EntityHandle icoSphere1 = viewer.getEntity("icoSphere1");
+    assert(icoSphere1.valid());
+    {
+        auto phong = std::dynamic_pointer_cast<rcube::BlinnPhongMaterial>(
+            icoSphere1.get<rcube::Drawable>()->material);
+        phong->diffuse_color = glm::vec3(0.0, 0.3, 0.7);
+    }
     viewer.execute();
     return 0;
 }

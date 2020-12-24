17d5e0442e4066782a968e008c415fc23bc20e03
pradeep-pyro <pradeep.pyro@gmail.com>
2020-12-24 18:44:53 -0500

    Remove unavailable colormaps for now

--- include/RCubeViewer/Colormap.h
+++ include/RCubeViewer/Colormap.h
@@ -8,12 +8,8 @@ namespace rcube
 
 enum class Colormap
 {
-    Viridis = 0,
-    Plasma = 1,
-    Magma = 2,
-    Inferno = 3,
-    Jet = 4,
-    Grays = 5
+    Viridis,
+    Magma,
 };
 
 void colormap(Colormap cm, float value, float vmin, float vmax, glm::vec3 &rgb);

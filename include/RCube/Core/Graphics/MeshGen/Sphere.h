14f3e8b012da7a1b746ef17e6e3f91490c299d76
pradeep-pyro <pradeep.pyro@gmail.com>
2021-01-08 21:32:06 -0500

    Add uv sphere

--- include/RCube/Core/Graphics/MeshGen/Sphere.h
+++ include/RCube/Core/Graphics/MeshGen/Sphere.h
@@ -9,6 +9,8 @@ TriangleMeshData icoSphere(float radius = 1, unsigned int subdivisions = 0);
 
 TriangleMeshData cubeSphere(float radius = 1, unsigned int n_segments = 1);
 
+TriangleMeshData uvSphere(float radius, unsigned int long_segments, unsigned int lat_segments);
+
 } // namespace rcube
 
 #endif // SPHERE_H
--- src/RCube/Core/Graphics/MeshGen/Sphere.cpp
+++ src/RCube/Core/Graphics/MeshGen/Sphere.cpp
@@ -36,9 +36,9 @@ TriangleMeshData icoSphere(float radius, unsigned int subdivisions)
             glm::vec3 bc = (b + c) / 2.f;
             glm::vec3 ca = (c + a) / 2.f;
             verts.insert(verts.end(), {ab, bc, ca});
-            unsigned int ij = verts.size() - 3;
-            unsigned int jk = verts.size() - 2;
-            unsigned int ki = verts.size() - 1;
+            unsigned int ij = unsigned int(verts.size()) - 3;
+            unsigned int jk = unsigned int(verts.size()) - 2;
+            unsigned int ki = unsigned int(verts.size()) - 1;
             faces.insert(faces.end(), {{i, ij, ki}, {ij, j, jk}, {ki, jk, k}});
             faces[idx] = {jk, ki, ij};
         }
@@ -72,6 +72,63 @@ TriangleMeshData cubeSphere(float radius, unsigned int n_segments)
         glm::vec3 norm_v = glm::normalize(data.vertices[i]);
         data.vertices[i] = norm_v * radius;
         data.normals[i] = norm_v;
+    }
+    return data;
+}
+
+TriangleMeshData uvSphere(float radius, unsigned int long_segments, unsigned int lat_segments)
+{
+    // Based on http://www.songho.ca/opengl/gl_sphere.html
+    TriangleMeshData data;
+    data.indexed = true;
+    float lat_step = glm::two_pi<float>() / lat_segments;
+    float long_step = glm::pi<float>() / long_segments;
+    float inv_radius = 1.0f / radius;
+    for (unsigned int i = 0; i <= long_segments; ++i)
+    {
+        float long_angle = glm::half_pi<float>() - i * long_step; // starting from pi/2 to -pi/2
+        float xy = radius * std::cos(long_angle);                 // r * cos(u)
+        float z = radius * std::sin(long_angle);                  // r * sin(u)
+
+        for (unsigned int j = 0; j <= lat_segments; ++j)
+        {
+            float lat_angle = j * lat_step; // starting from 0 to 2pi
+
+            float x = xy * std::cos(lat_angle); // r * cos(u) * cos(v)
+            float y = xy * std::sin(lat_angle); // r * cos(u) * sin(v)
+            data.vertices.push_back({x, z, y});
+
+            float nx = x * inv_radius;
+            float ny = z * inv_radius;
+            float nz = y * inv_radius;
+            data.normals.push_back({nx, ny, nz});
+
+            float u = (float)j / lat_segments;
+            float v = (float)i / long_segments;
+            data.texcoords.push_back({u, v});
+        }
+    }
+
+    for (unsigned int i = 0; i < long_segments; ++i)
+    {
+        unsigned int k1 = i * (lat_segments + 1); // beginning of current stack
+        unsigned int k2 = k1 + lat_segments + 1;  // beginning of next stack
+
+        for (unsigned int j = 0; j < lat_segments; ++j, ++k1, ++k2)
+        {
+            // 2 triangles per sector excluding first and last stacks
+            // k1 => k2 => k1+1
+            if (i != 0)
+            {
+                data.indices.push_back({k1, k2, k1 + 1});
+            }
+
+            // k1+1 => k2 => k2+1
+            if (i != (long_segments - 1))
+            {
+                data.indices.push_back({k1 + 1, k2, k2 + 1});
+            }
+        }
     }
     return data;
 }
